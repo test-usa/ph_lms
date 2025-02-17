d6bbb916a847fd374c5e08cd7c333a8c2f054757
Rahman Abdul Quadir <rahmanaq777@gmail.com>
2025-02-17 15:16:27 +0600

    bug fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MainModule } from './main/main.module';
-import { ContentService } from './main/content/content.service';
 import { DbModule } from './db/db.module';
-
+import { ContentController } from './main/content/content.controller';
+import { ContentService } from './main/content/content.service';
+import { DbService } from './db/db.service';
 
 @Module({
   imports: [MainModule, DbModule],
-  controllers: [AppController],
-  providers: [AppService, ContentService],
+  controllers: [AppController, ContentController],
+  providers: [AppService, ContentService, DbService],
 })
 export class AppModule {}
