8ab8a31af8bdce20021e73a514aa7af6115506d1
Rahman Abdul Quadir <rahmanaq777@gmail.com>
2025-02-18 16:25:43 +0600

    ISSUE SORTED

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ContentModule } from './content/content.module';
-import { CourseModule } from './course/course.module';
+import { ModuleController } from './module/module.controller';
+import { ModuleService } from './module/module.service';
 import { ModuleModule } from './module/module.module';
+import { DbService } from 'src/db/db.service';
 
 @Module({
-  imports: [AuthModule, ContentModule, CourseModule, ModuleModule],
+  imports: [AuthModule, ContentModule, ModuleModule],
+  controllers: [ModuleController],
+  providers: [ModuleService, DbService],
 })
 export class MainModule {}
