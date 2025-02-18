0f8643534160ad6cffed1fb1d2bcdd3729da2324
Rahman Abdul Quadir <rahmanaq777@gmail.com>
2025-02-18 14:38:30 +0600

    random test

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ContentModule } from './content/content.module';
-<<<<<<< HEAD
 import { ModuleController } from './module/module.controller';
 import { ModuleService } from './module/module.service';
 import { ModuleModule } from './module/module.module';
@@ -11,10 +10,5 @@ import { DbService } from 'src/db/db.service';
   imports: [AuthModule, ContentModule, ModuleModule],
   controllers: [ModuleController],
   providers: [ModuleService, DbService],
-=======
-
-@Module({
-  imports: [AuthModule, ContentModule],
->>>>>>> 831a5ea848c9347351b8c30df298d2b3ac9220bd
 })
 export class MainModule {}
