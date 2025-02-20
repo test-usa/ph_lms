9d61547323917cf6f62bfbedbef1a4dbb0740486
bdcalling <bdcalling@bdcallings-Mac-mini-2.local>
2025-02-20 11:37:16 +0600

    billing modeule initieated

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -5,8 +5,9 @@ import { ModuleModule } from './module/module.module';
 import { UserModule } from './user/user.module';
 import { CourseModule } from './course/course.module';
 import { QuizModule } from './quiz/quiz.module';
+import { BillingModule } from './billing/billing.module';
 
 @Module({
-  imports: [AuthModule, ContentModule, CourseModule, ModuleModule, UserModule, QuizModule],
+  imports: [AuthModule, ContentModule, CourseModule, ModuleModule, UserModule, QuizModule, BillingModule],
 })
 export class MainModule {}
