9927d278b8c8ee75a5e9d041d431c0d50a0482e3
Rahman Abdul Quadir <rahmanaq777@gmail.com>
2025-02-24 14:37:13 +0600

    blog module setup done

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -6,8 +6,9 @@ import { UserModule } from './user/user.module';
 import { QuizModule } from './quiz/quiz.module';
 import { AdminModule } from './admin/admin.module';
 import { CourseModule } from './course/course.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [AuthModule, ContentModule, CourseModule, ModuleModule, UserModule, QuizModule, AdminModule],
+  imports: [AuthModule, ContentModule, CourseModule, ModuleModule, UserModule, QuizModule, AdminModule, BlogModule],
 })
 export class MainModule {}
