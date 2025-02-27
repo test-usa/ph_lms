2ce1a4a2e550bb8c538a354fa39db163bdccb905
bdcalling <bdcalling@bdcallings-Mac-mini-2.local>
2025-02-27 12:29:28 +0600

    shift to codespace

--- src/main/content/content.service.ts
+++ src/main/content/content.service.ts
@@ -25,7 +25,6 @@ export class ContentService {
         title: dto.title,
         video: dto.video,
         description: dto.description,
-        assignment: dto.assignment,
         moduleId: dto.moduleId,
       },
     });
