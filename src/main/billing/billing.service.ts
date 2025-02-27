7bfc3617c508e19c3882d3e8bb6f253e6641ea99
bdcalling <bdcalling@bdcallings-Mac-mini-2.local>
2025-02-27 15:36:11 +0600

    minor dix

--- src/main/billing/billing.service.ts
+++ src/main/billing/billing.service.ts
@@ -12,7 +12,7 @@ export class BillingService {
         private readonly configService: ConfigService,
     ){
         this.stripe = new Stripe(this.configService.getOrThrow<string>('STRIPE_SECRET_KEY'), {
-            apiVersion: '2025-02-24.acacia',
+            apiVersion: '2025-01-27.acacia',
           });
     }
 
