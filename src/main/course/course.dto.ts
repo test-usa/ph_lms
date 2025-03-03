5588a1c6c1c203b0bf3ffd3d188736dc5ccc722b
Md Bakhtiar Fahim <bakhtiarfahim360@gmail.com>
2025-03-03 10:30:40 +0600

    add

--- src/main/course/course.dto.ts
+++ src/main/course/course.dto.ts
@@ -3,16 +3,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNumber, IsUUID, IsBoolean } from 'class-validator';
 
 export class CreateCourseDto {
-  @ApiProperty({
-    description: 'Title of the product',
-    example: 'Gaming Laptop',
-  })
   @IsString()
   title: string;
 
-
-
-  @ApiProperty({ description: 'Price of the product', example: 1299.99 })
   @IsNumber()
   price: number;
 }
